b2287bed380078ddae568f037ceabb4f976b3fb9
Serap BERAN <79447748+serapberan@users.noreply.github.com>
2023-06-13 23:16:56 +0300

    Add files via upload

---
+++ MQTT/Mqtt/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Text;
+
+/*
+ MQTT -> MQTT(Message Queuing Telemetry Transport)
+ Düşük bant genişliğine sahip  basit bir mesajlaşma protokolüdür.
+ MQTT iletişimi bir publish ve subscribe sistemi olarak çalışır. 
+ Cihazlar belirli bir konuda mesajlar yayınlar. 
+ Veriler direk Broker üzerinden gönderilmez. 
+ Bir Topic aracılığı ile gönderilir. 
+ Buna abone olan tüm cihazlar mesajı alır. 
+
+MQTT’de temel kavramLAR
+Publish(Yayınlama) - Subscribe(Abone Olma)
+Topics (Konular):yayınlanan mesajların yönlendirildiği konuları ifade eder.Konular, kesirli ("/") ayrılmış bir dize yapısına sahiptir 
+Broker(Sunucu)mesajları yönlendirir ve iletimini sağlar.
+Messages(Veriler) UTF-8 kodlamasıyla kodlanmış bir dize veya byte dizisi olarak gönderilir.
+
+..........(QoS) Mesaj Gönderim Seviyesi...... 
+MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE --> (bir kez iletme) 
+MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE (en fazla bir kez iletme) 
+MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE (en az bir kez iletme) bulunur.
+
+..........Nuget.............
+M2Mqtt Paketini kurmamız gerekir
+
+namespaceleri
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+-
+
+ */
+
+
+#region  Mqtt ile (Publisher) veri gönderim işlemi 
+namespace MqttPublish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Baglantı işlemleri
+            MqttClient mqttClient = new MqttClient("broker.hivemq.com");
+
+            //clientId --> Random atadık 
+            string clientId = Guid.NewGuid().ToString();
+            mqttClient.Connect(clientId);
+
+            //Mesaj içeriği
+            Console.WriteLine("Gönderilecek Mesajınızı Giriniz: ");
+            string message = Console.ReadLine();
+            
+            //Topic -> Yayn yapılacak işlem konusu
+            //Yayınlama işlemi için Topic Bilgisi
+            string Topic = "test/deneme";
+
+            //Yayınlama İşlemi
+            //Mesaj Byte Çevrilir
+            //MqttMsgBase --> Mesaj Gönderim seviyesini ifade eder
+            //Mesaj abone olan kişiye gönderilecek ve son veri iletilecektir.
+            mqttClient.Publish(Topic, Encoding.UTF8.GetBytes(message), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+
+        }
+    }
+
+}
+#endregion
---
+++ MQTT/MqttSubscribe/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Text;
+
+
+#region Mqtt ile (Subscribe) gelen veriyi almak ve işlemek için kullnılmıştır.
+class Programm
+{
+  
+    static void Main(string[] args)
+    {
+        //Baglantı işlemleri
+        MqttClient mqttClient = new MqttClient("broker.hivemq.com");
+        //Mesaj geldiğinde işlem tetikler 
+        mqttClient.MqttMsgPublishReceived += client_recievedMessage;
+
+        //clientId --> Random atadık 
+        string clientId = Guid.NewGuid().ToString();
+        mqttClient.Connect(clientId);
+
+        //Yyaınlanan mesajı alma işlemi 
+        //String[] { "test/deneme" } --> hangi konuya abone olacağını belirtiyoruz ,çokluda olabilir
+        //MqttMsgBase --> Mesaj Gönderim seviyesini ifade eder
+        //Mesaj abone olan kişiye gönderilecek ve son veri iletilecektir.
+        mqttClient.Subscribe(new String[] { "test/deneme" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+    }
+
+    /// <summary>
+    /// Mesaj yayınlandığında tetiklenir
+    /// </summary>
+    /// <param name="sender"> Olayı tetikleyen (client)</param>
+    /// <param name="e">e.Message baytlarını bir dizeye dönüştürür.
+    /// gelen mesajı okunabilir bir metin formatına dönüştürmek için kullanılır.<br/></param>
+    static void client_recievedMessage(object sender, MqttMsgPublishEventArgs e)
+    {
+        var message = System.Text.Encoding.Default.GetString(e.Message);
+        System.Console.WriteLine("Alınan Mesaj: " + message);
+    }
+}
+#endregion
